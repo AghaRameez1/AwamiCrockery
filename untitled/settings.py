c7b4ff6226bf62b842c9451b01f5611a2f9c7ac9
AghaRameez1 <37932292+AghaRameez1@users.noreply.github.com>
2018-08-23 18:38:42 +0500

    -Test

--- untitled/settings.py
+++ untitled/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -23,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7$r$4@a2!7u8rq7nd1_2cliujcse6=_2@h5mpr=a20-ah@+3^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://awamicrockery.herokuapp.com/','*']
+ALLOWED_HOSTS = ['*.herokuapp.com']
 
 
 # Application definition
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'untitled.urls'
@@ -78,11 +81,7 @@ WSGI_APPLICATION = 'untitled.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'awamicrockery',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'default': dj_database_url.config(default=dj_database_url.config('DATABASE_URL'))
     }
 }
 
@@ -122,22 +121,25 @@ USE_TZ = True
 
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__)) + '/..'
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join('static'),
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__)) + '/..'
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
+    os.path.join('static'),
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
+#
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join('static'),
+#     # os.path.join(PROJECT_ROOT, 'static'),
+# )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    from untitled.local_settings import *
